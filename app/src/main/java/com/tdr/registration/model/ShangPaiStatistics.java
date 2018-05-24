0733df02243f878a02a00126b4b7fe37aae123de
KingJA <kingjavip@gmail.com>
2018-05-24 14:22:25 +0800

    添加上牌查询

---
+++ app/src/main/java/com/tdr/registration/model/ShangPaiStatistics.java
@@ -0,0 +1,175 @@
+package com.tdr.registration.model;
+
+/**
+ * Description:TODO
+ * Create Time:2018/5/24 13:43
+ * Author:KingJA
+ * Email:kingjavip@gmail.com
+ */
+public class ShangPaiStatistics {
+
+
+    /**
+     * ID : 618D9E54-F8B0-4B7B-A389-39596F6CA849
+     * LOGIN : admin
+     * TYPE : Police
+     * USERNAME : 管理员
+     * UnitId : 37180C76-669F-845A-5E05-36801A8C0935
+     * ManageUnitId : null
+     * unitname : null
+     * unittype : null
+     * unitno : null
+     * sort : null
+     * has_platenumber : 0//上牌总数
+     * no_theftno : 0//上牌未转防盗登记数
+     * has_theftno : 0//上牌转防盗登记数
+     * new_has_theftno : null
+     * ele_insatall : null
+     * total : null
+     */
+
+    private String ID;
+    private String LOGIN;
+    private String TYPE;
+    private String USERNAME;
+    private String UnitId;
+    private Object ManageUnitId;
+    private Object unitname;
+    private Object unittype;
+    private Object unitno;
+    private Object sort;
+    private String has_platenumber;
+    private String no_theftno;
+    private String has_theftno;
+    private Object new_has_theftno;
+    private Object ele_insatall;
+    private Object total;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getLOGIN() {
+        return LOGIN;
+    }
+
+    public void setLOGIN(String LOGIN) {
+        this.LOGIN = LOGIN;
+    }
+
+    public String getTYPE() {
+        return TYPE;
+    }
+
+    public void setTYPE(String TYPE) {
+        this.TYPE = TYPE;
+    }
+
+    public String getUSERNAME() {
+        return USERNAME;
+    }
+
+    public void setUSERNAME(String USERNAME) {
+        this.USERNAME = USERNAME;
+    }
+
+    public String getUnitId() {
+        return UnitId;
+    }
+
+    public void setUnitId(String UnitId) {
+        this.UnitId = UnitId;
+    }
+
+    public Object getManageUnitId() {
+        return ManageUnitId;
+    }
+
+    public void setManageUnitId(Object ManageUnitId) {
+        this.ManageUnitId = ManageUnitId;
+    }
+
+    public Object getUnitname() {
+        return unitname;
+    }
+
+    public void setUnitname(Object unitname) {
+        this.unitname = unitname;
+    }
+
+    public Object getUnittype() {
+        return unittype;
+    }
+
+    public void setUnittype(Object unittype) {
+        this.unittype = unittype;
+    }
+
+    public Object getUnitno() {
+        return unitno;
+    }
+
+    public void setUnitno(Object unitno) {
+        this.unitno = unitno;
+    }
+
+    public Object getSort() {
+        return sort;
+    }
+
+    public void setSort(Object sort) {
+        this.sort = sort;
+    }
+
+    public String getHas_platenumber() {
+        return has_platenumber;
+    }
+
+    public void setHas_platenumber(String has_platenumber) {
+        this.has_platenumber = has_platenumber;
+    }
+
+    public String getNo_theftno() {
+        return no_theftno;
+    }
+
+    public void setNo_theftno(String no_theftno) {
+        this.no_theftno = no_theftno;
+    }
+
+    public String getHas_theftno() {
+        return has_theftno;
+    }
+
+    public void setHas_theftno(String has_theftno) {
+        this.has_theftno = has_theftno;
+    }
+
+    public Object getNew_has_theftno() {
+        return new_has_theftno;
+    }
+
+    public void setNew_has_theftno(Object new_has_theftno) {
+        this.new_has_theftno = new_has_theftno;
+    }
+
+    public Object getEle_insatall() {
+        return ele_insatall;
+    }
+
+    public void setEle_insatall(Object ele_insatall) {
+        this.ele_insatall = ele_insatall;
+    }
+
+    public Object getTotal() {
+        return total;
+    }
+
+    public void setTotal(Object total) {
+        this.total = total;
+    }
+}
