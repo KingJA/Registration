a1aefe970ea5ab01921d8b163de963e4194428eb
KingJA <kingjavip@gmail.com>
2018-06-27 16:56:15 +0800

    1096基本完成

--- app/src/main/java/com/tdr/registration/util/Constants.java
+++ app/src/main/java/com/tdr/registration/util/Constants.java
@@ -27,10 +27,10 @@ public class Constants {
     /**
      * Webservice参数
      */
-    public static final String WEBSERVER_URL = "https://ykgcapp.iotone.cn/OpenService.asmx";// WebServices访问地址，正式服
+//    public static final String WEBSERVER_URL = "https://ykgcapp.iotone.cn/OpenService.asmx";// WebServices1访问地址，正式服
 //    public static final String WEBSERVER_URL = "http://183.129.130.119:48012/OpenService.asmx";// WebServices访问地址，测试====>外网
 //    public static final String WEBSERVER_URL = "http://10.130.0.104:8012/OpenService.asmx";// WebServices访问地址，测试====>测试内网
-//    public static final String WEBSERVER_URL = "http://183.129.130.119:58012/OpenService.asmx";// WebServices访问地址，测试====>测试外网
+    public static final String WEBSERVER_URL = "http://183.129.130.119:58012/OpenService.asmx";// WebServices访问地址，测试====>测试外网
 //    public static final String WEBSERVER_URL = "http://10.130.0.104:8071/OpenService.asmx";// WebServices访问地址，测试
 //    public static final String WEBSERVER_URL = "http://10.130.0.104:8812/OpenService.asmx";// WebServices访问地址，测试
 //    public static final String WEBSERVER_URL = "http://183.129.130.119:52000/OpenService.asmx";// WebServices访问地址，测试
